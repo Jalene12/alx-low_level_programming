023da6d2c127c2a88e681142d71f7c8a24dcd835
Jalene12 <jalenetadesse12@gmail.com>
2023-06-15 18:19:56 -0700

    "Programming is like building a multilingual puzzle, followed by a new line.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
-#!/bin/bash
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - Entry point 
- * Return: Always 0 (Success)
+ * main - prints "Programming is like building a multilingual puzzle, followed by a new line.
+ * Return: Always 0.
  */
 int main(void)
 {
